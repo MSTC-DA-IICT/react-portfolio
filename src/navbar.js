50140093663bdae5351ac167933fd25d967c4fbf
Harsh <harshmp492@gmail.com>
2021-10-02 19:29:22 +0530

    created navbar and pending the work for small screen(JS)

--- src/navbar.js
+++ src/navbar.js
@@ -1,21 +1,31 @@
+import './css/Navbar.css'
 function Navbar(){
     return(
         <>
         {/* NAVBAR */}
-            <nav className="navbar" id="navbar">
-            <div className="navmain container">
-                <div className="logo"><span>#ME</span></div>
-                <ul className="menu" id="toggle-menu">
-                <li><a href="#home">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#services">Services</a></li>
-                <li><a href="#skills">Skills</a></li>
-                <li><a href="#projects">Projects</a></li>
-                <li><a href="#contact">Contact</a></li>
-                </ul>
-            </div>
-            </nav>
-            <hr></hr>
+        <nav className="navbar" >
+        <div className="navmain" id="navmain">
+        
+          <div className="logo"><span>#ME</span></div>
+          <a href="#" className="toggle-btn"> {/*onclick method pending*/}
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+          </a>
+          <div>
+          <div id="navbar-links">
+          <ul className="menu" id="toggle-menu">
+            <li><a href="#home" className="active">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#skills">Skills</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+          </div>
+          </div>
+        </div>
+      </nav>
         </>
     );
 }
