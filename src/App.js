ffb6d939c12439fa1918f40008e7544470a71322
Harsh <harshmp492@gmail.com>
2021-10-02 19:02:27 +0530

    Created Navbar and partially Done Responsivness

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import './css/App.css';
-
+import './css/Navbar.css'
 function App() {
+
+  
+  
   return (
+    
     <>
+    
       {/* NAVBAR */}
-      <nav className="navbar" id="navbar">
-        <div className="navmain container">
+      <nav className="navbar" >
+        <div className="navmain" id="navmain">
+        
           <div className="logo"><span>#ME</span></div>
+          <a href="#" className="toggle-btn"> {/*onclick method pending*/}
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+          </a>
+          <div>
+          <div id="navbar-links">
           <ul className="menu" id="toggle-menu">
-            <li><a href="#home">Home</a></li>
+            <li><a href="#home" className="active">Home</a></li>
             <li><a href="#about">About</a></li>
             <li><a href="#services">Services</a></li>
             <li><a href="#skills">Skills</a></li>
             <li><a href="#projects">Projects</a></li>
             <li><a href="#contact">Contact</a></li>
           </ul>
+          </div>
+          </div>
         </div>
       </nav>
-      <hr></hr>
-
 
       {/* MAIN */}
       <section className="main" id="main">
