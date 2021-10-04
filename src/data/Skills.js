fc13cb6a2b415d9d77b58cb842f08e393ae16735
Priyansh Vyas <f20201388@goa.bits-pilani.ac.in>
2021-10-04 23:46:35 +0530

    added progress bar in skills section

---
+++ src/data/Skills.js
@@ -0,0 +1,34 @@
+/* 
+to add a new skill, add a new array to the end of the "Skills" array and :-
+-  put your skill as the first element (String)
+-  put your percentage as the second element (String, followed by % symbol)
+just like the data entered below.
+*/
+
+const Skills = [
+    [
+        "HTML" , "90%"
+    ],
+    [
+        "CSS" , "70%"
+    ],
+    [
+        "JavaScript" , "75%"
+    ],
+    [
+        "ReactJs" , "60%"
+    ],
+    [
+        "Express.Js" , "80%"
+    ],
+    [
+        "MongoDB" , "80%"
+    ],
+    [
+        "Firebase" , "60%"
+    ],
+]
+
+
+
+export default Skills
