eabc32e9189ee321b5e71d2ae6195dfc52c9621d
TheDeveloperD <91713424+TheDeveloperD@users.noreply.github.com>
2021-10-04 08:28:16 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,15 +2,18 @@ function Main(){
     return(
         <>
             {/* MAIN */}
+      
         <section className="main" id="main">
-            <div className="container">
-            <div className="main-content">
+            <div className="main-container">
+              <div className="main-content">
                 <h1 className="text-1">Hello, I am </h1>
                 <h1 className="text-2">#ME</h1>
-            </div>
+              </div>
+              <div className="main-image">
+                <img src={mainImage} alt="image" className="image" />
+              </div>
             </div>
         </section>
-        <hr></hr>
         </>
     );
 }
