55c5597a4579991eed364ab2154afbf42d4ba260
TheDeveloperD <91713424+TheDeveloperD@users.noreply.github.com>
2021-10-04 15:39:31 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,26 +1,22 @@
-import mainimg from './image/mainimg.jpg'
-function Main(){
-    return(
-        <>
-            {/* MAIN */}
-      
-        <section className="main" id="main">
-            <div className="main-container">
-              <div className="center">
-              <span id="lineintro"></span>
-              </div>
-              <div className="main-content">
-                <h1 className="text-1">Hello, I am </h1>
-                <h1 className="text-2">#ME</h1>
-              </div>
-              <div className="main-image">
-                <img src={mainimg} alt="image" className="image" />
-              </div>
-{/* IMG credits: Character vector created by vectorjuice - www.freepik.com  */}
-            </div>
-        </section>
-        </>
-    );
+import mainImage from './image/main-image.png';
+function Main() {
+  return (
+    <>
+      {/* MAIN */}
+
+      <section className="main" id="main">
+        <div className="main-container">
+          <div className="main-content">
+            <h1 className="text-1">Hello, I am </h1>
+            <h1 className="text-2">#ME</h1>
+          </div>
+          <div className="main-image">
+            <img src={mainImage} alt="image" className="image" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default Main;
