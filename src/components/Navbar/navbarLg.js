3d3b57b5b7b82a589a9dab557518792c1610b528
pratikartik <kartikshikhare26@gmail.com>
2021-10-27 20:03:25 +0530

    Created new logo

--- src/components/Navbar/navbarLg.js
+++ src/components/Navbar/navbarLg.js
@@ -1,28 +1,49 @@
-import '../../css/Navbar.css'
-function NavbarLg(){
-    return(
-        <>
-        {/* NAVBAR */}
-        <nav className="navbar" >
-        <div className="navmain" id="navmain">
-          <div className="logo">
-          <i className="top-left"></i>
-          <span><img src={process.env.PUBLIC_URL + '/images/logo1.png'} alt="logo" className="logoimg"/></span>
-          <i className="top-right"></i></div>
-          <div id="shadow-border">
-          <ul className="menu" id="toggle-menu">
-            <li><a href="#home" className="active">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#services">Services</a></li>
-            <li><a href="#skills">Skills</a></li>
-            <li><a href="#projects">Projects</a></li>
-            <li><a href="#contact">Contact</a></li>
-          </ul>
-          </div>
-        </div>
-      </nav>
-        </>
-    );
+import "../../css/Navbar.css";
+function NavbarLg() {
+	return (
+		<>
+			{/* NAVBAR */}
+			<nav className="navbar">
+				<div className="navmain" id="navmain">
+					<div className="logo">
+						<i className="top-left"></i>
+						<span>
+							<img
+								src={process.env.PUBLIC_URL + "/images/logo1.svg"}
+								alt="logo"
+								className="logoimg"
+							/>
+						</span>
+						<i className="top-right"></i>
+					</div>
+					<div id="shadow-border">
+						<ul className="menu" id="toggle-menu">
+							<li>
+								<a href="#home" className="active">
+									Home
+								</a>
+							</li>
+							<li>
+								<a href="#about">About</a>
+							</li>
+							<li>
+								<a href="#services">Services</a>
+							</li>
+							<li>
+								<a href="#skills">Skills</a>
+							</li>
+							<li>
+								<a href="#projects">Projects</a>
+							</li>
+							<li>
+								<a href="#contact">Contact</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</>
+	);
 }
 
 export default NavbarLg;
